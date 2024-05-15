c5db4289cbdd6c10a75e8eace639f95a7dcdae42
LostLukas <lukasdorji@gmail.com>
2024-05-15 23:55:04 +0200

    Add: Easyeffects

--- configuration.nix
+++ configuration.nix
@@ -283,6 +283,7 @@
     nix-output-monitor
     nvd
     wget
+    easyeffects
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   ];
 
