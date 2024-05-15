196131aa30b57d0bd5a568465b26d7a9487c06e1
LostLukas <lukasdorji@gmail.com>
2024-05-15 16:24:14 +0200

    Enable autodiscovery of network printers

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,11 @@
   
     };
     printing.enable = true; # Enable CUPS to print documents.
+    avahi = { # Enable autodiscovery of network printers
+      enable = true;
+      nssmdns = true;
+      openFirewall = true;
+    };
     libinput.enable = true;
     displayManager.sddm = {
       enable = true;
