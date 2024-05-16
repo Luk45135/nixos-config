4c1c372926375243a77a390e18c4266eea0a02af
LostLukas <lukasdorji@gmail.com>
2024-05-16 15:22:10 +0200

    rm: removing easyeffects again because it breaks waybar tool and has overall little benefit for me currently

--- configuration.nix
+++ configuration.nix
@@ -283,7 +283,6 @@
     nix-output-monitor
     nvd
     wget
-    easyeffects
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   ];
 
