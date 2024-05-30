8f3ebed07bca7cd89bcaff82f68738f7c21c672a
LostLukas <lukasdorji@gmail.com>
2024-05-30 23:24:24 +0200

    Nvidia: Change from mkDriver to beta for 555

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -13,16 +13,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver { # Install 555 like this until its merged into nixos-unstable
-      version = "555.42.02";
-      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-      sha256_aarch64 = lib.fakeSha256;
-      openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      persistencedSha256 = lib.fakeSha256;
-    
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     #package = config.boot.kernelPackages.nvidiaPackages.mkDriver { # Stable 535 Drivers
     #  version = "535.154.05";
     #  sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
