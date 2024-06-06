25e9d114dcb8555ba200c9ce361b1cc2b8278b1b
LostLukas <lukasdorji@gmail.com>
2024-06-06 22:39:02 +0200

    mpv: accelerated video playback settings

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -124,6 +124,11 @@
         ytdl-format = "bestvideo+bestaudio";
         ao = "pulse";
         audio-device = "auto";
+        hwdec = "auto-safe";
+        vo = "gpu";
+        profile = "gpu-hq";
+        gpu-context = "wayland";
+        force-window = true;
       };
     };
     git = {
