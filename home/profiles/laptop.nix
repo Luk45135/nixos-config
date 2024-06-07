f77e48c5239d345a9e6af35bddcf2d41beeb5bbe
LostLukas <lukasdorji@gmail.com>
2024-06-08 00:44:47 +0200

    dconf: button-layout and color-scheme

--- home/profiles/laptop.nix
+++ home/profiles/laptop.nix
@@ -31,6 +31,15 @@
     ];
   };
 
+  dconf.settings = {
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = ":minimize,maximize,close";
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
