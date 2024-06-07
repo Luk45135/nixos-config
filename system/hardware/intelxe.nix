a8987717a0f7052d80228df8a7b8e4b5c48da958
LostLukas <lukasdorji@gmail.com>
2024-06-07 23:31:55 +0200

    fix: xe driver as kernelModule

--- system/hardware/intelxe.nix
+++ system/hardware/intelxe.nix
@@ -1,23 +1,23 @@
 { config, pkgs, ... }: {
   
-  boot.kernelPackages = pkgs.linuxPackages_latest; # Xe drivers only work on 6.8 and up
-  hardware = {
-    intelgpu.driver = "xe";
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        intel-vaapi-drivers
-        libvdpau-va-gl
-        interl-media-driver
-      ];
-      extraPackages32 = with pkgs.driversi686Linux; [
-        intel-vaapi-drivers
-        libvdpau-va-gl
-        interl-media-driver
-      ];
-    };
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest; # Xe drivers only work on 6.8 and up
+    initrd.kernelModules = [ "xe" ];
+  };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-vaapi-drivers
+      libvdpau-va-gl
+      interl-media-driver
+    ];
+    extraPackages32 = with pkgs.driversi686Linux; [
+      intel-vaapi-drivers
+      libvdpau-va-gl
+      interl-media-driver
+    ];
   };
 
 
