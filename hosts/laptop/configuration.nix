3f004697594f04154acfc9094206029b15e9b3f3
LostLukas <lukasdorji@gmail.com>
2024-06-08 14:02:45 +0200

    Add-laptop: window-gestures

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -82,6 +82,7 @@
 
     gnomeExtensions.gsconnect
     gnomeExtensions.tray-icons-reloaded
+    gnomeExtensions.window-gestures
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
