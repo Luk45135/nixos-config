9062ecc188d299929288d7eaa291ec7452a31cf0
LostLukas <lukasdorji@gmail.com>
2024-06-08 12:42:45 +0200

    fix: syncthing: nobara-hp -> nixhp and id

--- system/services/syncthing.nix
+++ system/services/syncthing.nix
@@ -10,13 +10,13 @@
     settings = {
       devices = {
         "fedora-server" = { id = "RRKARZM-CQJRN7C-KO77MSF-R6WB5QM-7T6XR7I-QI7TQ63-SDQCNTR-JS33TQC"; };
-        "nobara-hp" = { id = "BZ5MUTO-IFSONN7-RCNUHC3-WKZX2EV-FRBLOJK-HMFWJ2R-BUVSTOE-XRZSKQA"; };
+        "nixhp" = { id = "JOI7ZEZ-25OMYGJ-CMSF6O4-YBITCQL-FDJX734-55JNYZK-JHGNAWQ-2WKF4AP"; };
         "SM-G985F" = { id = "CHN5RFJ-ULJB47F-5ZXALZ5-G76SS5C-4PEUPIB-5P4AV6N-DH752CE-SLPKRAY"; };
       };
       folders = {
         "Syncthing" = {
           path = "/home/lukasd/Documents/Syncthing";
-          devices = [ "fedora-server" "nobara-hp" "SM-G985F" ];
+          devices = [ "fedora-server" "nixhp" "SM-G985F" ];
         };
         "Music" = {
           path = "/home/lukasd/Music";
