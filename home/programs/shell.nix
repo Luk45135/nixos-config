b1e6047001fd0a2c40f9ed9cc1346bf77cc4eee8
LostLukas <lukasdorji@gmail.com>
2024-07-01 22:12:43 +0200

    fix indenting

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -67,12 +67,12 @@
         catppuccin = {
           src = pkgs.fetchFromGitHub {
             owner = "catppuccin";
-	    repo = "bat";
-	    rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
-	    sha256 = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
-	  };
-        file = "themes/Catppuccin Mocha.tmTheme";
-	};
+	          repo = "bat";
+	          rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
+	          sha256 = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
+	        };
+            file = "themes/Catppuccin Mocha.tmTheme";
+	      };
       };
       extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch ];
     };
