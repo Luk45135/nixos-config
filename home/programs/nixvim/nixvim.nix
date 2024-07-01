4328753d02b0cf5e2f5ec941f671c3fa24f48af5
LostLukas <lukasdorji@gmail.com>
2024-07-01 19:44:44 +0200

    add lsp nixd and treesitter

--- home/programs/nixvim/nixvim.nix
+++ home/programs/nixvim/nixvim.nix
@@ -7,8 +7,19 @@
   programs.nixvim = {
     enable = true;
 
+    globals = {
+      mapleader = " ";
+      maplocalleader = " ";
+    };
     colorschemes.catppuccin.enable = true;
     plugins = {
+      lsp = {
+	enable = true;
+	servers = {
+	  nixd.enable = true;
+	};
+      };
+      treesitter.enable = true;
       telescope.enable = true;
       lightline.enable = true;
     };
