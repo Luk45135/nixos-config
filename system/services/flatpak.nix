93fc9b7037f7f9cb3b84d900397d2b2d15b94ece
LostLukas <lukasdorji@gmail.com>
2024-07-02 22:15:21 +0200

    flatpak: adding flathub repo

--- system/services/flatpak.nix
+++ system/services/flatpak.nix
@@ -1,6 +1,10 @@
 {
   services.flatpak = {
     enable = true;
+    remotes = [{
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }];
     packages = [
 
     ];
