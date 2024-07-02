e937d25f3a9deab597c5f787728abe43343a9f22
LostLukas <lukasdorji@gmail.com>
2024-07-02 18:03:35 +0200

    FiraCode -> JetBrainsMono

--- system/core/fonts.nix
+++ system/core/fonts.nix
@@ -3,6 +3,6 @@
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ];})
+    (nerdfonts.override { fonts = [ "JetBrainsMono" "DroidSansMono" ];})
   ];
 }
