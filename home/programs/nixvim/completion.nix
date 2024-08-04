2c3bb272c17b57f12b1040b65f00ecce91afed9b
LostLukas <lukasdorji@gmail.com>
2024-08-04 10:08:32 +0200

    nvim completions: use autoEnableSources and convert mappings to use nix style instead of raw

--- home/programs/nixvim/completion.nix
+++ home/programs/nixvim/completion.nix
@@ -5,30 +5,18 @@
       enable = true;
       settings = {
         mapping = {
-          __raw = ''
-            cmp.mapping.preset.insert({
-              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-              ['<C-f>'] = cmp.mapping.scroll_docs(4),
-              ['<C-Space>'] = cmp.mapping.complete(),
-              ['<C-e>'] = cmp.mapping.abort(),
-              ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            })
-          '';
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
         snippet = {
           expand = "function(args) require('luasnip').lsp_expand(args.body) end";
         };
-        sources = {
-          __raw = ''
-            cmp.config.sources({
-              { name = 'nvim_lsp' },
-              { name = 'luasnip' },
-              { name = 'ultisnips' },
-            }, {
-              { name = 'buffer' },
-            })
-          '';
-        }; 
+        autoEnableSources = true;
       };
     };
     cmp-nvim-lsp.enable = true;
