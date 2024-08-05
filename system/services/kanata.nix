5ae455e6dcf2cf861bb0fbc45e6cb9a0be39b434
LostLukas <lukasdorji@gmail.com>
2024-08-05 22:46:44 +0200

    kanata: add laptop keyboard

--- system/services/kanata.nix
+++ system/services/kanata.nix
@@ -6,6 +6,7 @@
       devices = [
         "/dev/input/by-id/usb-SONiX_USB_DEVICE-event-kbd"
         "/dev/input/by-id/usb-SONiX_USB_DEVICE-event-if01"
+        "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
       ];
       config = ''
         (defsrc
