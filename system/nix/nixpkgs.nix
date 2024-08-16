cc6944ced5c811c5afc95fc68839b23960744bf8
LostLukas <lukasdorji@gmail.com>
2024-08-16 02:40:11 +0200

    switch walker cache

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -4,11 +4,11 @@
     experimental-features = ["nix-command" "flakes"]; # Enabling nix commands and flakes
     substituters = [
       "https://nix-gaming.cachix.org"
-      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
     ];
     trusted-public-keys = [
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
     ];
   };
   nixpkgs.config.allowUnfree = true;
