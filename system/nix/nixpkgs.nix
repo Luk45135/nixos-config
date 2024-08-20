d441e32349969d911da6d16fca6a205c19789877
LostLukas <lukasdorji@gmail.com>
2024-08-20 23:51:37 +0200

    rm: don't need walker on cosmic

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -4,12 +4,10 @@
     experimental-features = ["nix-command" "flakes"]; # Enabling nix commands and flakes
     substituters = [
       "https://nix-gaming.cachix.org"
-      "https://walker.cachix.org"
       "https://cosmic.cachix.org"
     ];
     trusted-public-keys = [
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
       "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
   };
