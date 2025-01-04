cffcd29e2331cf28273f1f8ae2c6e19fd465a4a3
LostLukas <lukasdorji@gmail.com>
2025-01-04 04:09:40 +0100

    fix: `hardware.pulseaudio` has been renamed to `services.pulseaudio`

--- system/services/pipewire.nix
+++ system/services/pipewire.nix
@@ -3,7 +3,7 @@
     inputs.nix-gaming.nixosModules.pipewireLowLatency
   ];
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
