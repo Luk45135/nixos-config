e818df175d821c550147cc6ba8843fda447ed425
LostLukas <lukasdorji@gmail.com>
2025-01-15 22:44:45 +0100

    temp: fix home-manager kdeconnect-indicator service
    https://github.com/nix-community/home-manager/issues/6191

--- home/services/kdeconnect.nix
+++ home/services/kdeconnect.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   services.kdeconnect = {
     enable = true;
     indicator = true;
     package = pkgs.kdePackages.kdeconnect-kde;
   };
+  systemd.user.services.kdeconnect-indicator.Unit.Requires = lib.mkForce []; # https://github.com/nix-community/home-manager/issues/6191
 }
