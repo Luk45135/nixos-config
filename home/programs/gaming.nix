20add58766bab52a106e499fd21791c101a4a179
LostLukas <lukasdorji@gmail.com>
2025-04-26 14:05:31 +0200

    change: definie pipewire latency with a override
    the afore mantioned override

--- home/programs/gaming.nix
+++ home/programs/gaming.nix
@@ -13,7 +13,7 @@
     dolphin-emu
     cemu
     ryujinx
-    inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
+    (inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override {pipewire_latency = "48/48000";})
     steamtinkerlaunch
   ];
 }
