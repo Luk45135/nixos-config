b59ab4f1a8924989b6a4d1d19fa842c3bec6b09c
LostLukas <lukasdorji@gmail.com>
2025-04-30 20:58:14 +0200

    add: enable Scanning support and add simple-scan

--- system/services/printing.nix
+++ system/services/printing.nix
@@ -1,11 +1,12 @@
-{
+{pkgs, ...}: {
   services = {
     printing.enable = true; # Enable CUPS to print documents.
-    avahi = {
-      # Enable autodiscovery of network printers
+    avahi = { # Enable autodiscovery of network devices
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
     };
   };
+  hardware.sane.enable = true; # Enable SANE for scanning support
+  environment.systemPackages = [pkgs.simple-scan];
 }
