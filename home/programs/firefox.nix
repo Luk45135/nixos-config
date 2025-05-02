6f981ba627098726f1c43a34892be77a890670fe
LostLukas <lukasdorji@gmail.com>
2025-05-02 10:21:19 +0200

    change: slim down firefox config

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -5,67 +5,29 @@
 }: {
   programs.firefox = {
     enable = true;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
-      DisablePocket = true;
-      DisableFirefoxAccounts = true;
-      DisableAccounts = true;
-      DisableFirefoxScreenshots = true;
-      Preferences = {
-        "browser.startup.homepage" = "https://homepage.local.lukastech.top";
-      };
-    };
     profiles.lukasd = {
       search = {
         force = true;
-        # default = "Startpage - English";
+        default = "SearXNG";
         engines = {
-          "Nix Packages" = {
-            urls = [
-              {
-                template = "https://search.nixos.org/packages";
-                params = [
-                  {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
-                  }
-                ];
-              }
-            ];
-
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@np"];
+          "SearXNG" = {
+            urls = [{ template = "https://searxng.local.lukastech.top/?q={searchTerms}"; }];
+            iconMapObj."16" = "https://searxng.local.lukastech.top/favicon.ico";
+            definedAliases = ["@sx"];
           };
         };
       };
-
+      settings = {
+        "browser.startup.homepage" = "https://homepage.local.lukastech.top";
+      };
       extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
         bitwarden
-        buster-captcha-solver
-        clearurls
-        decentraleyes
-        #enhancer-for-youtube
+        # enhancer-for-youtube
         fastforwardteam
-        mal-sync
-        omnivore
         return-youtube-dislikes
         sponsorblock
-        startpage-private-search
         greasemonkey
-        translate-web-pages
         ublock-origin
-        user-agent-string-switcher
       ];
     };
   };
