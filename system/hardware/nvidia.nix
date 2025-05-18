cef8f063df34cb612d48058cbd46d2f734d30741
LostLukas <lukasdorji@gmail.com>
2025-05-18 18:04:21 +0200

    rm: unneccesaty comments

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -19,13 +19,5 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.latest;
-    #package = config.boot.kernelPackages.nvidiaPackages.mkDriver { # Stable 535 Drivers
-    #  version = "535.154.05";
-    #  sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-    #  sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-    #  openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-    #  settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-    #  persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-    #};
   };
 }
