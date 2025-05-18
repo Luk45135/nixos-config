5084005cd9b03e81b46fb542652868af959decf7
LostLukas <lukasdorji@gmail.com>
2025-05-18 18:04:44 +0200

    add(steam): add proton-ge as compat package

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -1,9 +1,10 @@
-{...}: {
+{ pkgs, ...}: {
   programs = {
     gamemode.enable = true; # Enable Gamemode
     steam = {
       enable = true;
       gamescopeSession.enable = true;
+      extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
   };
 }
