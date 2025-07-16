81121f74d572b050d980b6ff8264f560090cb0e6
LostLukas <lukasdorji@gmail.com>
2025-07-16 23:08:13 +0200

    add(git): sign git commits and tags by default

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,9 +1,15 @@
+{ config, ... }:
 {
   programs = {
     git = {
       enable = true;
       userName = "LostLukas";
       userEmail = "lukasdorji@gmail.com";
+      signing = {
+        signByDefault = true;
+        format = "ssh";
+        key = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      };
       extraConfig = {
         init.defaultBranch = "main";
       };
